68a7dddde0a13f8716867e23bfc57799b371a630
SeiPanda <70478113+SeiPanda@users.noreply.github.com>
2024-06-21 22:39:04 +0200

    Fixed asset folder creation

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { writeFile, readFile } from 'node:fs/promises';
+import { writeFile, readFile, mkdir } from 'node:fs/promises';
 
 @Injectable()
 export class ImageService {
   readonly imagePath: string = 'assets/images/';
   async save(image: Express.Multer.File): Promise<string> {
     const imageName = `${Date.now()}_${image.originalname}`;
+    await mkdir(`${this.imagePath}`, { recursive: true });
     await writeFile(`${this.imagePath}${imageName}`, image.buffer, 'binary');
     return imageName;
   }
